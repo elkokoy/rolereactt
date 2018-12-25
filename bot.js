790a80fcbea6dfea55c250fb3734371c6f8213f6
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-25 08:01:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,69 @@
-const Eris = require("eris");
-var bot = new Eris("NTIzNjQ0MjMxOTY1NzM3MDAw.Dvchgg.TzSSHODNsRXp9RVuzXCaquIlz20");
-//** لاتعدل على شي تحت ذا **//
-bot.on("ready", () => {
-onstart();
+//Settings!
+const yourID = ""; //Instructions on how to get this: https://redd.it/40zgse
+const setupCMD = "!createrolemessage"
+let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
+const roles = ["Hacker", "Artist", "Public Relations", "Intern"];
+const reactions = ["💻", "🖌", "😃", "🆕"];
+const botToken = ""; /*You'll have to set this yourself; read more
+                     here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token*/
+
+//Load up the bot...
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login(botToken);
+
+//If there isn't a reaction for every role, scold the user!
+if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+
+//Function to generate the role messages, based on your settings
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); //DONT CHANGE THIS
+    return messages;
+}
+
+
+bot.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
+            });
+        }
+    }
+})
+
+
+bot.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = bot.channels.get(event.d.channel_id);
+        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+       
+            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+            var role = msg.content.match(re)[1];
+        
+            if (user.id != bot.user.id){
+                var roleObj = msg.guild.roles.find(r => r.name === role);
+                var memberObj = msg.guild.members.get(user.id);
+                
+                if (event.t === "MESSAGE_REACTION_ADD"){
+                    memberObj.addRole(roleObj)
+                } else {
+                    memberObj.removeRole(roleObj);
+                }
+            }
+        }
+        })
+ 
+    }   
 });
-
-function onstart(){
-
-setInterval(function(){
-
-var currentTime = new Date(),
-hours = currentTime.getHours() + 0 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-bot.editChannel("523645547479957504", { name : "●⌠ " + "Time : " + hours + ":" + minutes + " " + suffix + " ⌡●"});
-bot.editChannel("523645440265027585", { name : "●⌠ " + "Users : " + bot.users.size + " ⌡●"});
-bot.editChannel("523645402382073867", { name : "●⌠ " + "Servers : " + bot.guilds. + " ⌡●"});
-bot.editChannel("523645471248482324", { name : "✲"});
-bot.editChannel("523645471248482324", { name : "✲W"});
-bot.editChannel("523645471248482324", { name : "✲We"});
-bot.editChannel("523645471248482324", { name : "✲Wel"});
-bot.editChannel("523645471248482324", { name : "✲Welc"});
-bot.editChannel("523645471248482324", { name : "✲Welco"});
-bot.editChannel("523645471248482324", { name : "✲Welcom"});
-bot.editChannel("523645471248482324", { name : "✲Welcome"});
-bot.editChannel("523645471248482324", { name : "✲Welcome "});
-bot.editChannel("523645471248482324", { name : "✲Welcome T"});
-bot.editChannel("523645471248482324", { name : "✲Welcome To"});
-bot.editChannel("523645471248482324", { name : "✲Welcome To "});
-bot.editChannel("523645471248482324", { name : "✲Welcome To ℊ"});
-bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯ"});
-bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ"});
-bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ𝖪"});
-bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ𝖪𝒴"});
-bot.editChannel("523645471248482324", { name : "✲Welcome To ℊℯℯ𝖪𝒴✲"});
-
-
-
-
-
-}, 4000);
-
-}
-
-bot.connect();
+client.login(process.env.BOT_TOKEN);
